d34a881e6c95de04c6c07ba225b6af379f4ad8b5
Igor-Technosoft <34654286+Igor-Technosoft@users.noreply.github.com>
2018-06-07 13:42:10 -0400

    Update TestRunner.java

--- src/test/java/framework/TestRunner.java
+++ src/test/java/framework/TestRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features"},
         // Specify tags to be executed
     
-        tags = {"@hotels-booking-page"},
+        tags = {""@ad-display""},
         
         //Specify step definition package name (Note: make sure to have this package on current directory) 
         glue = {"stepdefinition"}
